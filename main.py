fac3dc98848d1adf389ab04b9fc411e36c8b27dd
Ashutosh-kr7217 <krashutosh7217@gmail.com>
2024-10-07 23:43:11 +0530

    v1

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+# Create a FastAPI instance
+app = FastAPI()
+
+# Define a route for the root URL
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
